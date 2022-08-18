36c89b8eeb010020e42f39ee7789f666bdd9ec97
Navnath1989 <navanathhadapad@gmail.com>
2022-08-18 17:56:28 +0530

    Small Xml added

--- app/src/main/java/com/ondevice/mynewapp/ShowDataActivity.java
+++ app/src/main/java/com/ondevice/mynewapp/ShowDataActivity.java
@@ -10,12 +10,15 @@ import com.bumptech.glide.Glide;
 
 public class ShowDataActivity extends AppCompatActivity {
 
-    TextView txtShowData;
+    TextView txtShowData, tvSpecilCharRm;
     ImageView imgFullInfo;
 
-    String strText, strImage;
+    String strText, strImage, strFinalOt;
     public static final String abcdd = "abcd";
     public static final String images = "image";
+
+    String strSpecil = "It is a long established fact that a reader will be " +
+            "distracted by the # readable @ content ^ of a < > page  =  & $ when looking at its layout";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,14 @@ public class ShowDataActivity extends AppCompatActivity {
 
 
         txtShowData = findViewById(R.id.txtShowData);
-        txtShowData.setText(strText);
         imgFullInfo = findViewById(R.id.imgFullInfo);
+        tvSpecilCharRm = findViewById(R.id.tvSpecilCharRm);
+
+        txtShowData.setText(strText);
         Glide.with(this).load(strImage).into(imgFullInfo);
 
+        strFinalOt = strSpecil.replaceAll("\\W+"," ");
+
+        tvSpecilCharRm.setText(strFinalOt);
     }
 }
